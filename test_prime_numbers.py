484a368cad70c1f13fdd52a7a4a24df0b56eed17
Saravanabalagi R <saravanabalagi@hotmail.com>
2018-07-15 23:46:13 +0100

    Adding negative test cases

--- test_prime_numbers.py
+++ test_prime_numbers.py
@@ -1,6 +1,10 @@
 from prime_numbers import is_prime
 
+
 def test_is_prime():
-    assert is_prime(1) == False
-    assert is_prime(2) == True
-    assert is_prime(3) == True
+    assert is_prime(-1) is False
+    assert is_prime(0) is False
+
+    assert is_prime(1) is False
+    assert is_prime(2) is True
+    assert is_prime(3) is True
